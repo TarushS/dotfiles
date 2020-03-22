0837a77517f6be969a5f4a84ff008c1ad5ab1aad
TarushS <tsonakya@gmail.com>
2020-03-22 05:49:59 +0530

    script

--- script.sh
+++ script.sh
@@ -2,21 +2,24 @@
 
 # IKR THE SCRIPT IS CRUDE and wont be making things comlicated
 
-
 # updating packages and system
-pacman -Syu
-pacman -Syy
+sudo pacman -Syu
+sudo pacman -Syy
 
 # installing packages
-pacman -S - < pkglist.txt
-pacman -S tmux
+sudo pacman -S - < pkglist.txt
+sudo pacman -S tmux zsh
+sudo pacman -S kdecoration qt5-declarative qt5-x11extra cmake extra-cmake-modules
+sudo pacman -S cmake
 
 # installing sierrabreeze
-cd ..
-git clone https://github.com/kupiqu/SierraBreezeEnhanced
-cd SierraBreezeEnhanced
-chmod +x install.sh
-./install.sh
+# cd ..
+# git clone https://github.com/kupiqu/SierraBreezeEnhanced
+# cd SierraBreezeEnhanced
+# mkdir build && cd build
+# cmake .. -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_BUILD_TYPE=Release -DKDE_INSTALL_LIBDIR=lib -DBUILD_TESTING=OFF -DKDE_INSTALL_USE_QT_SYS_PATHS=ON
+# make
+# sudo make install
 
 # installing vmware tools
 arg=$1
@@ -37,20 +40,21 @@ then
 else
 
 # backing up and copying the dotfiles
-cd ~
 
-mv .zshrc .zshrc.bak
-mv .vim .vim.bak
-mv .vimrc .vimrc.bak
-mv .local .local.bak
-mv .config .config.bak
-mv .bashrc .bashrc.bak
+mv ~/.zshrc ~/.zshrc.bak
+mv ~/.vim ~/.vim.bak
+mv ~/.vimrc ~/.vimrc.bak
+mv ~/.local ~/.local.bak
+mv ~/.config ~/.config.bak
+mv ~/.bashrc ~/.bashrc.bak
 
 cp .zshrc ~
-cp .vim.tar.gz ~
 tar xvf .vim.tar.gz
+cp -r .vim ~
 cp .tmux.conf ~
 cp -r .local ~
 cp -r .config ~
 cp .bashrc ~
 cp .vimrc ~
+
+fi
